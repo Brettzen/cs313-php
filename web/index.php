29f7f52be10f08a203bc1afa65d1f822a16edca1
Brett Starks <brettz.starks@gmail.com>
2019-01-16 18:19:10 -0700

    Initial commit for cs313-php site

--- web/index.php
+++ web/index.php
@@ -1,5 +1,25 @@
 <?php
 
-  phpinfo();
+$action = filter_input(INPUT_POST, 'action');
+if($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if($action == NULL) {
+        $action = 'main';
+    }
+}
 
-?>
+
+switch($action) {
+    //actions
+    case 'assignments':
+        $assignment = true;
+        include 'view/assignments.php';
+        break;
+    
+    case 'main':
+    case 'about':
+    default: 
+        $assignment = false;
+        include 'view/main.php';
+}
+        
